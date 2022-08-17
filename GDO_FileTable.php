b162210d3b9c9a3711ed68ec94de58578f2f33e8
gizmore <gizmore@wechall.net>
2022-08-17 19:21:51 +0200

    '7.0.1-r1516 unit tests'

--- GDO_FileTable.php
+++ GDO_FileTable.php
@@ -13,7 +13,7 @@ use GDO\User\GDO_User;
  * Override gdoFileObjectTable() and return your GDO that shall have the files.
  * 
  * @author gizmore
- * @version 7.0.0
+ * @version 7.0.1
  * @since 6.1.0
  */
 class GDO_FileTable extends GDO
