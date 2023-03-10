384aea2902d1a5167caf36354d2028f17ba89f00
gizmore <gizmore@wechall.net>
2023-03-10 16:56:28 +0100

    '7.0.1-r1752'

--- GDT_Files.php
+++ GDT_Files.php
@@ -29,7 +29,7 @@ class GDT_Files extends GDT_File
 	### STUB GDT methods ###
 	########################
 	public function gdoColumnNames() : array { return GDT::EMPTY_ARRAY; } # NO DB column, we have a GDO_File table for this.
-	public function gdoColumnDefine() : string { return GDT::EMPTY_STRING; } # NO DB column. Your GDO_FileTable has the data.
+// 	public function gdoColumnDefine() : string { return GDT::EMPTY_STRING; } # NO DB column. Your GDO_FileTable has the data.
 	public function getGDOData() : array { return GDT::EMPTY_ARRAY; } # Only relation table. Handled by onCreate and onUpdate.
 // 	public function setGDOData(GDO $gdo=null) { return $this; }
 	
